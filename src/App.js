c6b891f957c3179481eb8de68cd1539ef2a71ff9
Alexandr <90519481+Sanb4ik@users.noreply.github.com>
2022-08-01 23:52:31 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useState } from "react";
+import "./style/App.css";
+function App() {
+  const [value, setValue] = useState();
+
+  return (
+    <div className="App">
+      <div className="post">
+        <div className="post__content">
+          <strong>1. JS</strong>       
+          <div>JS-ЯП</div>
+        </div>
+        <div className="post__btn">
+            <button>Delete</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
